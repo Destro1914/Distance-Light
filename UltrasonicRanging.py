4e28014312b38eab563d3e34b3f2802a234f0302
fletcher.bryce <fletcher.bryce@olsohio.org>
2021-03-25 13:00:02 -0400

    Update 3

--- UltrasonicRanging.py
+++ UltrasonicRanging.py
@@ -8,6 +8,12 @@
 import RPi.GPIO as GPIO
 import time
 
+from PCF8574 import PCF8574_GPIO
+from Adafruit_LCD1602 import Adafruit_CharLCD
+
+from time import sleep, strftime
+from datetime import datetime
+
 trigPin = 16
 echoPin = 18
 MAX_DISTANCE = 220          # define the maximum measuring distance, unit: cm
@@ -16,6 +22,7 @@ GPIO.setmode(GPIO.BOARD)  # use PHYSICAL GPIO Numbering
 GPIO.setup(trigPin, GPIO.OUT)  # set trigPin to OUTPUT mode
 GPIO.setup(echoPin, GPIO.IN)
 GPIO.setup(33, GPIO.OUT)
+
 
 def pulseIn(pin,level,timeOut): # obtain pulse time of a pin under timeOut
     t0 = time.time()
@@ -40,18 +47,46 @@ def getSonar():     # get the measurement results of ultrasonic module,with unit
 
 
 def distance_light():
+    PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+    PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
+
+    try:
+        mcp = PCF8574_GPIO(PCF8574_address)
+    except:
+        try:
+            mcp = PCF8574_GPIO(PCF8574A_address)
+        except:
+            print('I2C Address Error !')
+            exit(1)
+    # Create LCD, passing in MCP GPIO adapter.
+    lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4, 5, 6, 7], GPIO=mcp)
+    mcp.output(3, 1)  # turn on LCD backlight
+    lcd.begin(16, 2)
+    lcd.clear()
+
     while(True):
         distance = getSonar() # get distance
-
         print ("The distance is : %.2f Feet Away"%(distance))
-        time.sleep(.5)
+
+
         if distance<15 and distance > 0:
             print("light on")
             GPIO.output(33,GPIO.HIGH)
+
+            lcd.setCursor(0, 0)  # set cursor position
+            lcd.message('  Car Detected  ' + '\n')  # display CPU temperature
+            lcd.message("      "+str(round(distance))+" cm")  # display the time
+
             time.sleep(5)
+            lcd.clear()
         else :
             print ("light off")
             GPIO.output(33,GPIO.LOW)
+            # lcd.clear()
+            # lcd.setCursor(0, 0)  # set cursor position
+            # lcd.message('   Distance:' + '\n')  # display CPU temperature
+            # lcd.message("   " + str(round(distance, 2)))  # display the time
+            # time.sleep(.5)
 
 if __name__=="__main__":
     distance_light()
